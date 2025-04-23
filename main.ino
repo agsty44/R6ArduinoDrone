b1748077bd243f32dbeb03d8d56ac03ae614ba52
agsty44 <alexandiaingaming@gmail.com>
2025-04-23 12:37:21 +0100

    added code to handle writing to motors, only need USB serial/PS4 handling code now

--- main.ino
+++ main.ino
@@ -1,3 +1,19 @@
+/*
+
+PIN NUMBERS:
+
+LEFT MOTOR (CHANNEL A) : BRAKES PIN 9, DIRECTION PIN 12, SPEED PIN 3
+RIGHT MOTOR (CHANNEL B) : BRAKES PIN 8, DIRECTION PIN 11, SPEED PIN 8
+
+*/
+
+//GLOBAL VARS
+int leftDirPin = 12;
+int rightDirPin = 11;
+
+int leftSpeedPin = 3;
+int rightSpeedPin = 8;
+
 void setup() {
     //Disengage brake pins.
     pinMode(9, OUTPUT);
@@ -6,12 +22,38 @@ void setup() {
     digitalWrite(8, LOW);
 
     //Set direction pins
-    int leftDir = 12;
-    int rightDir = 11;
-    pinMode(leftDir, OUTPUT);
-    pinMode(rightDir, OUTPUT);
+    pinMode(leftDirPin, OUTPUT);
+    pinMode(rightDirPin, OUTPUT);
+
+    //Set speed pins
+    pinMode(leftSpeedPin, OUTPUT);
+    pinMode(rightSpeedPin, OUTPUT);
 }
 
 void loop() {
-    
+    //Read axis values
+    //TODO: IMPLEMENT USB SERIAL AND PS4 CONTROLLER LIBRARIES.
+    int leftStickYAxis;
+    int rightStickYAxis;
+
+    //Do a check to set pin dirs.
+    if (leftStickYAxis > 0) {
+	digitalWrite(leftDirPin, HIGH);
+    } else {
+        digitalWrite(leftDirPin, LOW);
+    }
+
+    if (rightStickYAxis > 0) {
+        digitalWrite(rightDirPin, HIGH);
+    } else {
+        digitalWrite(rightDirPin, LOW);
+    }
+
+    //Calculate the motor speed (this needs to solve to a value 0 -> 255)
+    int leftMotorSpeed = abs(leftStickYAxis) * 255;
+    int rightMotorSpeed = abs(rightStickYAxis) * 255;
+
+    //Now we need to output these values to the motors
+    analogWrite(leftSpeedPin, leftMotorSpeed);
+    analogWrite(rightSpeedPin, rightMotorSpeed);
 }
